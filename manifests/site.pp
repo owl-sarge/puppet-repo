0369c873b7294cd5a83effd82c4fe0439f010515
Poddubitki Sergey <owlpsv@gmail.com>
2023-12-12 21:17:11 +0300

    update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -13,11 +13,9 @@ node 'slave1.puppet' {
     ensure => latest,
     name   => httpd,
   }
-    
   service { 'httpd':
     ensure => running,
-  }
-
+  }  
   file { '/var/www/html/index.html':
     ensure => file,
     source => "/vagrant/files/index.html",
@@ -29,21 +27,17 @@ node 'slave2.puppet' {
     ensure => latest,
     name   => httpd,
   }
-  
   file { '/var/www/html/index.php':
     ensure => file,
     source => "/vagrant/files/index.php",
   }
-
   file { '/var/www/html/index.html':
     ensure => absent,
   }
-
   package { 'php':
     ensure => latest,
     name   => php,
   }
-
   service { 'httpd':
     ensure => running,
   }
