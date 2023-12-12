346a2d250f879d88a1930879733eed9536bfaf08
Poddubitki Sergey <owlpsv@gmail.com>
2023-12-12 21:10:06 +0300

    update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,41 +1,42 @@
 node 'master.puppet' {
-    package { 'nginx':
-      ensure => latest,
-      name   => nginx,
-    }
-    service { 'nginx':
-      ensure => running,
-    }
+  package { 'nginx':
+    ensure => latest,
+    name   => nginx,
+  }
+  service { 'nginx':
+    ensure => running,
+  }
 }
 
 node 'slave1.puppet','slave2.puppet' {
-    package { 'Apache':
-      ensure => latest,
-      name   => httpd,
-    }
-    
-    service { 'httpd':
-      ensure => running,
-    }
-
-node 'slave1.puppet' {
-    file { '/var/www/html/index.html':
-      ensure => file,
-      source => "/vagrant/files/index.html",
-    }
+  package { 'Apache':
+    ensure => latest,
+    name   => httpd,
   }
+    
+  service { 'httpd':
+    ensure => running,
+  }
+}
+node 'slave1.puppet' {
+  file { '/var/www/html/index.html':
+    ensure => file,
+    source => "/vagrant/files/index.html",
+  }
+}
 
 node 'slave2.puppet' {
-    file { '/var/www/html/index.php':
-      ensure => file,
-      source => "/vagrant/files/index.php",
-    }
+  file { '/var/www/html/index.php':
+    ensure => file,
+    source => "/vagrant/files/index.php",
+  }
 
-    file { '/var/www/html/index.html':
-      ensure => absent,
-    }
-    package { 'php':
-      ensure => latest,
-      name   => php,
-    }
+  file { '/var/www/html/index.html':
+    ensure => absent,
+  }
+
+  package { 'php':
+    ensure => latest,
+    name   => php,
+  }
 }
