f93a015702b0ea7d2988bc925ba5aba13c1e18ee
Poddubitki Sergey <owlpsv@gmail.com>
2023-12-12 23:21:52 +0300

    app site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,24 @@
 node 'slave2.puppet' {
-    package { 'Apache2':
-      ensure => latest,
-      name   => httpd,
-    }
-    service { 'httpd':
-      ensure => running,
-    }
+  package { 'Apache':
+    ensure => latest,
+    name   => httpd,
+  }
+  file { '/var/www/html/index.php':
+    ensure => file,
+    source => "/vagrant/files/index.php",
+  }
+  file { '/var/www/html/index.html':
+    ensure => absent,
+  }
+  package { 'php':
+    ensure => latest,
+    name   => php,
+  }
+  service { 'php':
+    name => php-fpm,
+    ensure => running,
+  }
+  service { 'httpd':
+    ensure => running,
+  }
 }
