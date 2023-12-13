ab3cd5401d24861b9667bcf18076c0f33db35e73
owl-sarge <150381713+owl-sarge@users.noreply.github.com>
2023-12-13 17:42:51 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -10,10 +10,10 @@ node 'slave1.puppet' {
     ensure => file,
     source => "/vagrant/files/index.html",
   }
-   file { '/var/www/html/static/':
+   file { '/var/www/static/':
     ensure => directory,
   }
-  file { '/var/www/html/static/index.html':
+  file { '/var/www/static/index.html':
     ensure => file,
     source => "/var/www/html/index.html",
   }
