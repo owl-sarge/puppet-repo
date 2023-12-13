b6821cf5b18629ec592aa783c9b7717bf15abde6
owl-sarge <150381713+owl-sarge@users.noreply.github.com>
2023-12-13 22:08:00 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,28 +3,25 @@ node 'slave2.puppet' {
     ensure => latest,
     name   => httpd,
   }
+  
+  package { 'php':
+    ensure => latest,
+  }
+  
   file { '/var/www/html/index.php':
     ensure => file,
     source => "/vagrant/files/index.php",
   }
+  
   file { '/var/www/html/index.html':
     ensure => absent,
   }
-  file { '/var/www/html/dynamic/':
-    ensure => directory,
-  }
-  file { '/var/www/html/dynamic/index.php':
-    ensure => file,
-    source => "/var/www/html/index.php",
-  }
-  package { 'php':
-    ensure => latest,
-    name   => php,
-  }
+  
   service { 'php':
     name => php-fpm,
     ensure => running,
   }
+  
   service { 'httpd':
     ensure => running,
   }
