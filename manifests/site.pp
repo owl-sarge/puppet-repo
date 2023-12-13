482f4405161a052d15306613c97cf81cf573f3d1
owl-sarge <150381713+owl-sarge@users.noreply.github.com>
2023-12-13 23:15:09 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,13 @@
 node 'master.puppet' {
-  package { 'nginx':
-    ensure => latest,
-    name   => nginx,
-  }
-
+include nginx
+nginx::resource::server {'static':
+  listen_port => 80,
+  proxy       => 'http://192.168.56.21',
+}
+nginx::resource::server {'dynamic':
+  listen_port => 80,
+  proxy       => 'http://192.168.56.22',
+}
   service { 'nginx':
     ensure => running,
   }
