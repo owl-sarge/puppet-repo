698e7428846996150363894e1844079a414611a2
owl-sarge <150381713+owl-sarge@users.noreply.github.com>
2023-12-14 18:22:05 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,11 @@
 class { 'apache': }
 
-apache::listen { '8000': }
+apache::vhost { 'localhost':
+  ip       => '127.0.0.1',
+  port     => '8000'
+  docroot  => '/var/www/first',
+  ip_based => true,
+}
 
 file { '/var/www/html/index.html':
     ensure => file,
