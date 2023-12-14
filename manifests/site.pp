878580f989e40001507b4681cd573d1e3f827bb1
owl-sarge <150381713+owl-sarge@users.noreply.github.com>
2023-12-14 18:15:07 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,8 @@
 class { 'apache': }
 
-apache::listen { '80': }
-
 apache::listen { '8000': }
 
 file { '/var/www/html/index.html':
     ensure => file,
     source => "/vagrant/files/index.html",
 }
-#service { 'httpd':
-#  ensure => running,
-#}
